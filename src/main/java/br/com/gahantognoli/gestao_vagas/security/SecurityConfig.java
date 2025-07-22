d9d4802a12d63df73706a0502283ae5bb42ae85b
Gabriel Antognoli <gabriel_antognoli@hotmail.com>
2025-07-21 22:25:18 -0300

    Adicionando Autenticação

--- src/main/java/br/com/gahantognoli/gestao_vagas/security/SecurityConfig.java
+++ src/main/java/br/com/gahantognoli/gestao_vagas/security/SecurityConfig.java
@@ -3,6 +3,8 @@ package br.com.gahantognoli.gestao_vagas.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -11,12 +13,17 @@ public class SecurityConfig {
   @Bean
   SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http.csrf((csrf) -> csrf.disable())
-      .authorizeHttpRequests(auth -> {
-        auth.requestMatchers("/candidate").permitAll();
-        auth.requestMatchers("/company").permitAll();
-        auth.anyRequest().authenticated();
-      });
+        .authorizeHttpRequests(auth -> {
+          auth.requestMatchers("/candidate").permitAll();
+          auth.requestMatchers("/company").permitAll();
+          auth.requestMatchers("/auth/company").permitAll();
+          auth.anyRequest().authenticated();
+        });
     return http.build();
   }
 
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
 }

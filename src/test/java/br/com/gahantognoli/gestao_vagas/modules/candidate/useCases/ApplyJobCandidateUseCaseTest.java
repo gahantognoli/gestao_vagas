31c63f4bede8b6ffd78a289fda4c03b13a8f2cbb
Gabriel Antognoli <gabriel_antognoli@hotmail.com>
2025-08-05 22:24:42 -0300

    Testes

---
+++ src/test/java/br/com/gahantognoli/gestao_vagas/modules/candidate/useCases/ApplyJobCandidateUseCaseTest.java
@@ -0,0 +1,58 @@
+package br.com.gahantognoli.gestao_vagas.modules.candidate.useCases;
+
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import br.com.gahantognoli.gestao_vagas.exceptions.JobNotFoundException;
+import br.com.gahantognoli.gestao_vagas.exceptions.UserNotFoundException;
+import br.com.gahantognoli.gestao_vagas.modules.candidate.CandidateEntity;
+import br.com.gahantognoli.gestao_vagas.modules.candidate.CandidateRepository;
+import br.com.gahantognoli.gestao_vagas.modules.company.repositories.JobRepository;
+
+@ExtendWith(MockitoExtension.class)
+public class ApplyJobCandidateUseCaseTest {
+  
+  @InjectMocks
+  private ApplyJobCandidateUseCase applyJobCandidateUseCase;
+
+  @Mock
+  private CandidateRepository candidateRepository;
+
+  @Mock
+  private JobRepository jobRepository;
+  
+  @Test
+  @DisplayName("Should not be able to apply job with candidate not found")
+  public void should_not_be_able_to_apply_job_with_candidate_not_found(){
+    try {
+      applyJobCandidateUseCase.execute(null, null);
+    } catch (Exception e) {
+      Assertions.assertThat(e).isInstanceOf(UserNotFoundException.class);
+    }
+  }
+
+  @Test
+  @DisplayName("Should not be able to apply job with job not found")
+  public void should_not_be_able_to_apply_job_with_job_not_found(){
+    try {
+      var idCandidate = UUID.randomUUID();
+      var candidate = new CandidateEntity();
+      candidate.setId(idCandidate);
+      when(candidateRepository.findById(idCandidate)).thenReturn(Optional.of(candidate));
+      applyJobCandidateUseCase.execute(idCandidate, null);
+    } catch (Exception e) {
+      Assertions.assertThat(e).isInstanceOf(JobNotFoundException.class);
+    }
+  }
+
+}
